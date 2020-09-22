c7526b7dc19b9f4390970c1a38950877b192efaf
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2020-09-22 14:53:13 -0400

    added beginnings of chart for AQI and created two columns for data display

--- results.js
+++ results.js
@@ -15,20 +15,40 @@ function showAir(city) {
     
     function show(data) {
         var airQuality = data.data[0].aqi
-        console.log(airQuality)
+        var airQualityInt = parseInt(airQuality)
+        console.log(airQualityInt)
+        console.log(typeof(airQualityInt))
 
-        if ("0" <= airQuality <= "50") {
+        if (0 <= airQualityInt && airQualityInt <= 50) {
             var green = airQuality.fontcolor("00fd00")
             return "<h3><strong>Air Quality Index</strong>: " + green
         }
 
-        else if ("51" <= airQuality <= "100") {
+        else if (51 <= airQualityInt && airQualityInt <= 100) {
             var yellow = airQuality.fontcolor("fdfd00")
             return "<h3><strong>Air Quality Index</strong>: " + yellow
         }
 
+        else if (101 <= airQualityInt && airQualityInt <= 150) {
+            var orange = airQuality.fontcolor("ff9500")
+            return "<h3><strong>Air Quality Index</strong>: " + orange
+        }
 
-        // return "<h3><strong>Air Quality Index</strong>: " + airQuality 
+        else if (151 <= airQualityInt && airQualityInt <= 200) {
+            var red = airQuality.fontcolor("ff0000")
+            return "<h3><strong>Air Quality Index</strong>: " + red
+        }
+
+        else if (201 <= airQualityInt && airQualityInt <= 251) {
+            var purple = airQuality.fontcolor("bb00fd")
+            return "<h3><strong>Air Quality Index</strong>: " + purple
+        }
+
+        else if (301 <= airQualityInt) {
+            var maroon = airQuality.fontcolor("740000")
+            return "<h3><strong>Air Quality Index</strong>: " + maroon
+        }
+
     }
 
 }
